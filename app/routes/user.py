4408bd2ab46dbb587ed9d7d123279344b51f2526
heitorfe <heitorfelix@yahoo.com.br>
2023-09-12 09:41:27 -0300

    add exception handling and logging

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,10 +1,23 @@
-from fastapi import  APIRouter
+from fastapi import  APIRouter, HTTPException
 from app.schemas.user import (
     FullUserProfile,
     MultipleUsersResponse,
     CreateUserResponse,
 )
 from app.services.user import UserService
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format =  '%(levelname)-6s %(name)-15s %(asctime)s.%(msecs)03d %(message)s',
+    datefmt= "%y-%m-%d %H:%M:%S",
+    filename = "log.txt",
+    )
+logger.setLevel(logging.INFO)
+
+console = logging.StreamHandler()
+logger.addHandler(console)
+
 
 def create_user_router() -> APIRouter:
     user_router = APIRouter(
@@ -20,8 +33,12 @@ def create_user_router() -> APIRouter:
         :param user_id: int - unique monotonically increasing integer id
         :return: FullUserProfile
         """
+        try:
+            full_user_profile = await user_service.get_user_info(user_id)
 
-        full_user_profile = await user_service.get_user_info(user_id)
+        except KeyError:
+            logger.error(f"Non-existent user_id : {user_id} was requested")
+            raise HTTPException(status_code=404, detail="User doesn't exist")
         return full_user_profile
 
     @user_router.put("/{user_id}")
@@ -31,7 +48,12 @@ def create_user_router() -> APIRouter:
 
     @user_router.delete("/{user_id}")
     async def remove_user(user_id: int):
-        await user_service.delete_user(user_id)
+        logger.info(f"About to delete user: {user_id}")
+        try:
+            await user_service.delete_user(user_id) 
+        except KeyError:
+            logger.error(f"Non-existent user_id : {user_id} was requested")
+            raise HTTPException(status_code=404, detail={"msg" : "User doesn't exist", "user_id" : user_id})
 
     @user_router.get("/all", response_model=MultipleUsersResponse)
     async def get_all_users_paginated(start: int = 0, limit: int = 2):
@@ -39,7 +61,7 @@ def create_user_router() -> APIRouter:
         formatted_users = MultipleUsersResponse(users=users)
         return formatted_users
 
-    @user_router.post("/", response_model = CreateUserResponse)
+    @user_router.post("/", response_model = CreateUserResponse, status_code=201)
     async def add_user(full_profile_info : FullUserProfile):
         user_id =  await user_service.create_update_user(full_profile_info)
         created_user = CreateUserResponse(user_id = user_id)
